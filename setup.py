4c52ea474c3e0e2fb14b9fb468129c8c5bec8f45
Mohammed Rifas S <39994235+RifasM@users.noreply.github.com>
2019-07-19 19:06:31 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ATG File Upload',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='rifas',
+    author_email='rifas2475@outlook.com',
+    description='Uses Google Drive API to upload files'
+)
+
+
